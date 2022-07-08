51cf6035b1a191c6a80e8b6d6c1db61335dbf7c6
Andy Guillermo Arellano Anleu <andy.arellano@office365.3pillarglobal.com>
2022-07-07 20:11:55 -0600

    Update

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export default function GifGrid({ category }) {
 
-    useEffect( () => {
-        getGifs(category);
-    },[  ])
+  const [images, setImages] = useState([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs( category );
+    setImages(newImages);
+  }
+
+  useEffect( () => {
+      getImages();
+  },[])
+
+  //   useEffect( () => {
+  //     getGifs(category)
+  //       .then( newImages => setImages(newImages))
+  // },[ ])
+
 
   return (
     <>
       <h3>{category}</h3>
+        <ol>
+          {/* Images.map */}
+          {
+          (images || []).map(image => (
+            <li>{image.title}</li>
+          ))}
+        </ol>
     </>
   );
-} 
+}
